d986a8f49ab9388d9ec76269b3b20a28076fb91f
Kevin Vega <a01612430@itesm.mx>
2021-11-05 23:20:40 -0600

    system_health topic pub and sub probed (all)

--- candidates_ws/src/integration/scripts/MechanismActionClient.py
+++ candidates_ws/src/integration/scripts/MechanismActionClient.py
@@ -1,30 +1,37 @@
 #!/usr/bin/env python3
 
+from io import StringIO
+from os import truncate
 from geometry_msgs import msg
 import roslib
 roslib.load_manifest('integration')
 import rospy
 import actionlib
 import random
+from std_msgs.msg import String
+from std_msgs.msg import UInt16
 from integration.msg import armAction, armGoal, neckAction, neckGoal, elevatorAction, elevatorGoal
-mechani
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
 if __name__ == '__main__':
-    rospy.init_node('Mechanism_Action_Client')
-    arm_client = actionlib.SimpleActionClient('arm_movement')
-    neck_client = actionlib.SimpleActionClient('neck_movement')
-    elevator_client = actionlib.SimpleActionClient('elevator_movement')
+    rospy.init_node('Mechanism_Action_Client', anonymous=True)
+    arm_client = actionlib.SimpleActionClient('arm_movement',armAction)
+    neck_client = actionlib.SimpleActionClient('neck_movement',neckAction)
+    elevator_client = actionlib.SimpleActionClient('elevator_movement',elevatorAction)
+    
 
-    print('Insert an option:')
+
+    print('Insert an option to make the simulation:')
     print('1 = arm:')
     print('2 = neck')
     print('3 = elevator')
-    option = input()
+    print('4 = system_healt')
+    option = int(input())
     
     while (option != 0):
+        print ('option = ',option)
         
 
         if option == 1:
@@ -35,7 +42,14 @@ if __name__ == '__main__':
             neck_goal = neckGoal()
             neck_goal = rospy.Subscriber('neck_movement', neckAction, callback)
         
+        elif option == 4:
+            rospy.Subscriber('system_health',UInt16, callback)
+            rospy.spin()
+            
+        
         elif option == 3:
             elevator_goal = elevatorGoal()
             elevator_goal = rospy.Subscriber('elevator_movement', elevatorAction, callback)
-        option = input('Insert an option: ')
+        option = int(input('Insert an option: '))
+        
+
--- candidates_ws/src/system_stimuli/scripts/shutdown_system.py
+++ candidates_ws/src/system_stimuli/scripts/shutdown_system.py
@@ -3,9 +3,23 @@
 import rospy
 import time
 from std_msgs.msg import UInt16
+from std_msgs.msg import String
+
+def talker():
+    pub = rospy.Publisher('system_health',UInt16, queue_size = 5)
+    rospy.init_node('mock_shutdown_system',anonymous=True)
+    rate = rospy.Rate(1)
+    option = 0
+    while not rospy.is_shutdown() and option == 0:
+        rospy.loginfo(0)
+        pub.publish(0)
+        rate.sleep()
+        #option = int(input())
+
 
 if __name__ == '__main__':
     try:
+        talker()
         rospy.init_node('mock_shutdown_system', anonymous=True)
         pub = rospy.Publisher('system_health', UInt16, queue_size=10)
         pub.publish(1)
